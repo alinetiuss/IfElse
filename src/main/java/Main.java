c522997a4e4482d3debe6bb8d63b196263269cb8
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-09-02 12:35:46 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int numerador = 16;
+    int denominador = 4;
+    int resto = numerador % denominador;
+
+    System.out.println(" O resto é: " + resto);
+    
+  }
+
+}
